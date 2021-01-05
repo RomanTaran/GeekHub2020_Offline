54b401b4d328d07ee30425d1bfec1507ff8425ce
roman <romantaran.taran@gmail.com>
2021-01-05 15:26:57 +0200

    Second edition

--- HomeWork5-6/Task1.js
+++ HomeWork5-6/Task1.js
@@ -5,14 +5,36 @@ function perform(value) {
   return param;
 }
 
-new Promise(function (resolve, reject) {
-  resolve(perform(20));
-}).then((result, a, b) => {
-  console.log(++result);
-  return result;
-}).then(result => {
-  if (result === 2) {
-    console.log(++result);
-    return result;
+const myPromise = function() {
+  this.allFunc = [];
+  this.lastResult;
+}
+myPromise.prototype.then = function (newFunc) {
+  this.allFunc.push(newFunc);
+}
+myPromise.prototype.resolve = function (arg) {
+  this.lastResult = arg;
+  for(let i = 0; i < this.allFunc.length; i++) {
+    const item = this.allFunc[i];
+    const res = item(this.lastResult);
+    if (res instanceof myPromise) {
+      const lastFuncs = this.allFunc.slice(i+1);
+      res.allFunc = lastFuncs;
+      break;
+    } else {
+      this.lastResult = res;
+    }
   }
+}
+
+const foo = new myPromise();
+
+foo.then((param)=>{
+  console.log(++param);
+  return param;
+});
+foo.then((param)=>{
+  console.log(++param);
+  return param;
 })
+foo.resolve(perform(20));
